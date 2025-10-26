f730db858c970b7bbe235edaefb91b760a0e3875
rezahtms <rezahtms@gmail.com>
2025-10-26 12:30:28 +0330

    feat(data-table):created custom data-table

---
+++ src/components/CustomDataTable.tsx
@@ -0,0 +1,184 @@
+"use client";
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ChevronDown } from "lucide-react";
+import * as React from "react";
+import { cn } from "./lib/utils";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Input } from "./ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+
+interface CustomDataTableProps<TData> {
+  title?: string;
+  data: TData[];
+  columns: ColumnDef<TData>[];
+  filterColumnKey?: string;
+  filterPlaceholder?: string;
+  emptyMessage?: string;
+}
+
+export function CustomDataTable<TData>({
+  title,
+  data,
+  columns,
+  filterColumnKey,
+  filterPlaceholder = "Filter...",
+  emptyMessage = "No results found.",
+}: CustomDataTableProps<TData>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  const filterColumn = filterColumnKey
+    ? table.getColumn(filterColumnKey)
+    : null;
+
+  return (
+    <div className="w-full border border-gray-300 rounded-lg p-2">
+      {title && (
+        <h2 className="text-xs text-tint-blue-500 font-semibold my-4">
+          {title}
+        </h2>
+      )}
+
+      <div className="flex items-center justify-between mb-2 border border-gray-300 rounded-lg p-2">
+        {filterColumn && (
+          <Input
+            placeholder={filterPlaceholder}
+            value={(filterColumn.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              filterColumn.setFilterValue(event.target.value)
+            }
+            className="max-w-xs text-[0.75rem] font-medium placeholder:text-xs placeholder:font-medium border border-gray-300 rounded-lg"
+          />
+        )}
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild className="select-none">
+            <Button variant="outline">
+              نمایش <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start" className="select-none">
+            {table
+              .getAllColumns()
+              .filter((col) => col.getCanHide())
+              .map((col) => (
+                <DropdownMenuCheckboxItem
+                  key={col.id}
+                  className="capitalize"
+                  checked={col.getIsVisible()}
+                  onCheckedChange={(value) => col.toggleVisibility(!!value)}
+                >
+                  {typeof col.columnDef.header === "string"
+                    ? col.columnDef.header
+                    : col.id}
+                </DropdownMenuCheckboxItem>
+              ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id} className="text-start">
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+
+          <TableBody>
+            {table.getRowModel().rows.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell className="text-xs" key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  {emptyMessage}
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div
+        id="pagination"
+        className={cn("flex items-center justify-center my-2")}
+      ></div>
+    </div>
+  );
+}
