a804e1b55eb51135b889b0a4682494122801daf0
rezahtms <rezahtms@gmail.com>
2025-11-03 16:37:25 +0330

    feat(shopping-card):created UserReceptionTime page

--- src/pages/home/shopping-card/ShoppingStep.tsx
+++ src/pages/home/shopping-card/ShoppingStep.tsx
@@ -3,14 +3,17 @@ import { useAppStore } from "@lib/stores/store";
 import ShoppingCardStepper from "../stepper/shoppingCardStepper";
 import UserAddress from "./UserAddress";
 import UserReceiveMethod from "./UserReceiveMethod";
+import UserReceiveTime from "./UserReceiveTime";
 
 export default function ShoppingStep() {
   const { currentStep } = useAppStore();
+
   return (
     <div className="w-full">
       <ShoppingCardStepper />
       {currentStep === "address" && <UserAddress />}
       {currentStep === "receiverMethod" && <UserReceiveMethod />}
+      {currentStep === "receiveTime" && <UserReceiveTime />}
     </div>
   );
 }
--- src/pages/home/shopping-card/UserReceiveMethod.tsx
+++ src/pages/home/shopping-card/UserReceiveMethod.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { DELIVERY_OPTIONS } from "@lib/constants/delivery-options";
+import { useAppStore } from "@lib/stores/store";
 import { useState } from "react";
 import DeliveryOptionCard from "./DeliveryOptionCard";
 import ShoppingCardPortalButton from "./ShoppingCardPortalButton";
 
 export default function UserReceiveMethod() {
   const [selected, setSelected] = useState<string>("express");
-
+  const { changeStepperState } = useAppStore();
   return (
     <form className="border border-gray-300 rounded-xl p-6 w-full bg-white shadow-sm">
       <h2 className="text-orange-500 text-base font-semibold mb-6 text-center">
@@ -24,7 +25,9 @@ export default function UserReceiveMethod() {
           />
         ))}
       </div>
-      <ShoppingCardPortalButton onClick={() => console.log("hello wrold")} />
+      <ShoppingCardPortalButton
+        onClick={() => changeStepperState("receiveTime")}
+      />
     </form>
   );
 }
