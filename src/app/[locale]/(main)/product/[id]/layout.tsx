9bfd912f03f83e15b3af44deb741b5549de8e75d
rezahtms <rezahtms@gmail.com>
2025-11-06 17:52:52 +0330

    fix(product):fixed product parallel route issue

--- src/app/[locale]/(main)/product/[id]/layout.tsx
+++ src/app/[locale]/(main)/product/[id]/layout.tsx
@@ -19,20 +19,23 @@ export async function generateMetadata({
 export default async function Layout({
   children,
   tabs,
+  params,
 }: {
   children: React.ReactNode;
   tabs: React.ReactNode;
+  params: Promise<{ id: string }>;
 }) {
+  const { id } = await params;
   return (
     <>
       <section>{children}</section>
       <Container>
         <div className="flex items-center gap-2 bg-tint-blue-100 py-3 px-4 text-xs text-tint-blue-500 font-medium rounded-tl-lg rounded-tr-lg ">
-          <Link href="/product/1/specification">مشخصات محصول</Link>
-          {/* <Link href="/product/1/specifications">نقد و بررسی</Link> */}
-          <Link href="/product/1/comment">نظرات کاربران</Link>
+          <Link href={`/product/${id}/specification`}>مشخصات محصول</Link>
+          <Link href={`/product/${id}/review`}>نقد و بررسی</Link>
+          <Link href={`/product/${id}/comment`}>نظرات کاربران</Link>
         </div>
-        <div className="mt-4">{tabs}</div>
+        <div className="mt-2">{tabs}</div>
       </Container>
     </>
   );
