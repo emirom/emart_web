fb356a6da0ddb85d332c102464723541b144c53c
rezahtms <rezahtms@gmail.com>
2025-11-22 18:24:41 +0330

    style(FormSwitchField):changed FormSwitchField styles

--- src/components/FormSwitchField.tsx
+++ src/components/FormSwitchField.tsx
@@ -48,10 +48,13 @@ const FormSwitchField = <T extends FieldValues>({
             <button
               type="button"
               onClick={toggle}
-              className="w-[120px] h-11 bg-white rounded-[7px] border border-[#D4F1F4] flex items-center transition-all duration-300 overflow-hidden relative"
+              className={cn(
+                "w-[120px] h-9 bg-white rounded-[7px] border border-input flex items-center transition-all duration-300 overflow-hidden relative",
+                fieldState.error && "border-destructive",
+              )}
             >
               <div
-                className={`absolute top-1 left-1 w-[calc(50%-0.25rem)] h-[calc(100%-0.5rem)] flex items-center justify-center text-white text-sm font-semibold rounded-[7px] transition-all duration-300
+                className={`absolute top-1 left-1 w-[calc(50%-0.25rem)] h-[calc(100%-0.5rem)] flex items-center justify-center text-white text-xs font-semibold rounded-[7px] transition-all duration-300
                   ${
                     isActive
                       ? "translate-x-[calc(50%+1.7rem)] bg-[#34C759] shadow-[0_0_8px_#34C759]"
@@ -60,7 +63,7 @@ const FormSwitchField = <T extends FieldValues>({
               >
                 {isActive ? "فعال" : "غیرفعال"}
               </div>
-              <div className="w-full h-full flex items-center justify-between px-1 text-sm font-normal text-center text-[#aaa]">
+              <div className="w-full h-full flex items-center justify-between px-1 text-xs font-normal text-center text-[#aaa]">
                 <span className="block mr-3">فعال</span>
                 <span>غیر فعال</span>
               </div>
