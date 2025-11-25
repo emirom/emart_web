e726a0ed2746e95e733e04df60b3f4dfe6e05040
rezahtms <rezahtms@gmail.com>
2025-11-25 16:09:34 +0330

    feat(store):created stores

---
+++ src/app/[locale]/(admin)/dashboard/stores/page.tsx
@@ -0,0 +1,42 @@
+import StoreTable from "@/pages/dashboard/stores/StoreTable";
+import { HeaderWithLink } from "@components/HeaderWithLink";
+import { TablePagination } from "@components/TablePagination";
+import { queryClient } from "@lib/apis/queryClient";
+import { ListStoresResponse } from "@lib/schemas";
+import { getStoreLocations } from "@lib/services/store-locations/store-locations";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "فروشگاه ها",
+  },
+  description: "لیست فروشگاه های فروشنده ها",
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string, string>>;
+}) {
+  const sp = searchParams ? await searchParams : {};
+  const page = Number(sp.page || 0);
+  const skip = page * 10;
+  const queryKey = ["/stores", { skip, limit: 10 }];
+  await queryClient.prefetchQuery({
+    queryKey: queryKey,
+    queryFn: () => getStoreLocations({ skip, limit: 10 }),
+  });
+  const cachedData = queryClient.getQueryData(queryKey) as ListStoresResponse;
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <HeaderWithLink
+        title="فروشگاه‌ها"
+        linkTitle="بازگشت"
+        linkHref="/dashboard"
+      />
+      <StoreTable data={cachedData} />
+      <TablePagination />
+    </HydrationBoundary>
+  );
+}
---
+++ src/pages/dashboard/stores/StoreTable.tsx
@@ -0,0 +1,102 @@
+"use client";
+import { CustomDataTable } from "@components/CustomDataTable";
+import { ListStoresResponse, Store } from "@lib/schemas";
+import { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+
+const columns: ColumnDef<Store>[] = [
+  {
+    accessorKey: "name",
+    header: "نام فروشگاه",
+  },
+  {
+    accessorKey: "slug",
+    header: "اسلاگ",
+  },
+  // {
+  //   accessorKey: "logo",
+  //   header: "لوگو",
+  //   cell: ({ row }) =>
+  //     row.original.logo ? (
+  //       <img
+  //         src={row.original.logo}
+  //         alt="لوگو"
+  //         style={{ width: 40, height: 40 }}
+  //       />
+  //     ) : (
+  //       "-"
+  //     ),
+  // },
+  {
+    accessorKey: "description",
+    header: "توضیحات",
+    cell: ({ row }) => row.original.description ?? "-",
+  },
+  {
+    accessorKey: "email",
+    header: "ایمیل",
+    cell: ({ row }) => row.original.email ?? "-",
+  },
+  {
+    accessorKey: "phone",
+    header: "تلفن",
+    cell: ({ row }) => row.original.phone ?? "-",
+  },
+  {
+    accessorKey: "website",
+    header: "وبسایت",
+    cell: ({ row }) =>
+      row.original.website ? (
+        <Link
+          href={row.original.website}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          🌐 {row.original.website}
+        </Link>
+      ) : (
+        "-"
+      ),
+  },
+  {
+    accessorKey: "isActive",
+    header: "فعال",
+    cell: ({ row }) => (row.original.isActive ? "✅" : "❌"),
+  },
+  {
+    accessorKey: "isApproved",
+    header: "تایید شده",
+    cell: ({ row }) => (row.original.isApproved ? "👍" : "⛔"),
+  },
+  {
+    accessorKey: "ownerId",
+    header: "شناسه مالک",
+    cell: ({ row }) => row.original.ownerId ?? "-",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "تاریخ ایجاد",
+    cell: ({ row }) =>
+      `📅 ${new Date(row.original.createdAt).toLocaleDateString("fa-IR")}`,
+  },
+  {
+    accessorKey: "updatedAt",
+    header: "آخرین بروزرسانی",
+    cell: ({ row }) =>
+      `📅 ${new Date(row.original.updatedAt).toLocaleDateString("fa-IR")}`,
+  },
+];
+
+export default function StoreTable({ data }: { data: ListStoresResponse }) {
+  return (
+    <CustomDataTable
+      title="فروشگاه‌ها"
+      data={data?.data}
+      columns={columns}
+      // filterConfigs={data?.filters}
+      emptyMessage="فروشگاهی یافت نشد"
+      filterColumnKey="name"
+      filterPlaceholder="نام فروشگاه را وارد نمایید"
+    />
+  );
+}
